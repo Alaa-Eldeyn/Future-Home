a869d423d7003f06fee5460eabb5fd1592e44afc
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2023-06-12 01:53:10 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
-        {/* <PrivateRoute path="/checkout" element={<Checkout />} /> */}
         <Route
           path="/checkout"
           element={
