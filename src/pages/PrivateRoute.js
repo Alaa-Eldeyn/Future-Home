f65e6ec57f9989a3f1b806cdfd21913975e492af
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2023-06-12 04:57:56 +0300

    update

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth0();
   if (!user) {
-    <Navigate to="/" />;
+    return <Navigate to="/" />;
   }
   return children;
 };
